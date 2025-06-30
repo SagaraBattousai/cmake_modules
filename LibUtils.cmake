5eab845104bfcb76dc9fafa0f3487a7932f51950
James Calo <jamesafcalo@gmail.com>
2025-06-30 01:35:11 +0100

    Finally fixed LibUtils to correctly get run automatically even if run for the irst time (as now creating the directory is part of the command and it only runs if it doesn't already exist. ResourceUtils is not properly done but it prints the hex for pasting into a file

--- LibUtils.cmake
+++ LibUtils.cmake
@@ -1,13 +1,9 @@
-# TODO: Why does this have to be run twice!!! crashes on first run ....
-#
 # Unfortunatly this must be called in the directory where the target is defined
 # so we can't get them all dynamically
 #
 # As post_build_copy_dlls_to_target only copies if the "target" is (re)built
 # this creates a (pseudo)target that will always run and copy the dlls
 #
-# Could use copy_if_different but it's definatly safer to always copy! How big
-# can dll's get?
 function(copy_dlls_to_target target)
   # Assigning to variable to clean up COMMENT code below and to aid with
   # whitespace
@@ -19,13 +15,15 @@ function(copy_dlls_to_target target)
   # TODO: Is this the nicest name?
   set(COPY_DLL_TARGET_NAME "COPY_DLLS_FOR_${target}")
 
-  add_custom_target(
-    ${COPY_DLL_TARGET_NAME}
+  # Can't use just POST_BUILD custom command as changes to dll's that do not require
+  # the target to be rebuilt will not be copied over.
+  add_custom_target(${COPY_DLL_TARGET_NAME}
+    # Required to ensure the directory exists :)
+    COMMAND ${CMAKE_COMMAND} -E make_directory $<TARGET_FILE_DIR:${target}>
     COMMAND ${CMAKE_COMMAND} -E copy -t $<TARGET_FILE_DIR:${target}>
             $<TARGET_RUNTIME_DLLS:${target}>
     COMMENT ${copy_comment}
     COMMAND_EXPAND_LISTS)
-
 
   # Get dependant libraries (unfortunatly not only SHARED libs
   # but shouldn't be an issue)
@@ -34,8 +32,9 @@ function(copy_dlls_to_target target)
   # Ensure targets are built *before* the DLL's are copied
   add_dependencies(${COPY_DLL_TARGET_NAME} ${target_libraries})
   
-  # Ensure executable has been built so the directory exists
-  add_dependencies(${COPY_DLL_TARGET_NAME} ${target})
+  # Ensure it's automatically called when target is built 
+  # (unfortunatly before but the above fixes that)
+  add_dependencies(${target} ${COPY_DLL_TARGET_NAME})
 
   # Store in separate folder
   set_target_properties(${COPY_DLL_TARGET_NAME}
@@ -43,16 +42,53 @@ function(copy_dlls_to_target target)
 
 endfunction()
 
+
+
+
+
+###############################
+## ORIGINAL FUNC WHICH HAS DEPENDENCY ISSUES
+
 # Unfortunatly this must be called in the directory where the target is defined
 # so we can't get them all dynamically
 #
-# Irritatingly if the "target" isn't (re)built then the (possibly) updated dll's
-# aren't copied so use symlink_dlls_to_target
-function(post_build_copy_dlls_to_target target)
-  add_custom_command(
-    TARGET ${target}
-    POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E copy -t $<TARGET_FILE_DIR:${target}>
-            $<TARGET_RUNTIME_DLLS:${target}>
-    COMMAND_EXPAND_LISTS)
-endfunction()
+# As post_build_copy_dlls_to_target only copies if the "target" is (re)built
+# this creates a (pseudo)target that will always run and copy the dlls
+#
+# Could use copy_if_different but it's definatly safer to always copy! How big
+# can dll's get?
+# function(copy_dlls_to_target target)
+#   # Assigning to variable to clean up COMMENT code below and to aid with
+#   # whitespace
+#   string(JOIN " " copy_comment "Copying the following dlls:"
+#          "[$<JOIN:$<PATH:GET_FILENAME,$<TARGET_RUNTIME_DLLS:${target}>>,, >]"
+#          "to $<TARGET_FILE_DIR:${target}>"
+#          "for Target: ${target}")
+
+#   # TODO: Is this the nicest name?
+#   set(COPY_DLL_TARGET_NAME "COPY_DLLS_FOR_${target}")
+
+#   add_custom_target(
+#     ${COPY_DLL_TARGET_NAME}
+#     COMMAND ${CMAKE_COMMAND} -E copy -t $<TARGET_FILE_DIR:${target}>
+#             $<TARGET_RUNTIME_DLLS:${target}>
+#     COMMENT ${copy_comment}
+#     COMMAND_EXPAND_LISTS)
+
+
+#   # Get dependant libraries (unfortunatly not only SHARED libs
+#   # but shouldn't be an issue)
+#   get_target_property(target_libraries ${target} LINK_LIBRARIES)
+
+#   # Ensure targets are built *before* the DLL's are copied
+#   add_dependencies(${COPY_DLL_TARGET_NAME} ${target_libraries})
+  
+#   # Ensure executable has been built so the directory exists but now this target isn't
+#   # auto built :P
+#   add_dependencies(${COPY_DLL_TARGET_NAME} ${target})
+
+#   # Store in separate folder
+#   set_target_properties(${COPY_DLL_TARGET_NAME}
+#                         PROPERTIES FOLDER "CmakeCopyDllsForTargets")
+
+# endfunction()
---
+++ ResourceUtils.cmake
@@ -0,0 +1,58 @@
+# TODO: Do it properly later but fornow well just print the formatted hex so we can copy and paste
+
+function(embed_file_as_macro file_path ouput_path macro_name)
+
+  set(options "")
+  set(oneValueArgs HEADER_GUARD)
+  set(multiValueArgs "")
+
+  # args as prefix is apparantly a good/correct choice according to cmake docs so ...
+  cmake_parse_arguments(PARSE_ARGV 3 args "${options}" "${oneValueArgs}"
+                        "${multiValueArgs}")
+
+  # Set HEADER_GUARD argument
+  if(NOT DEFINED args_HEADER_GUARD)
+    # No idea if this regex is correct (Trying to catch all at end, bit iffy but....)
+    string(REGEX REPLACE 
+      "([A-Za-z0-9_]*)\\.([A-Za-z0-9_])([A-Za-z0-9\\._\\-]*)" "__\\1_\\2__"
+     args_HEADER_GUARD "${args_HEADER_GUARD}")
+
+    string(TOUPPER "${args_HEADER_GUARD}" args_HEADER_GUARD)
+
+    if(HEADER_GUARD IN_LIST args_KEYWORDS_MISSING_VALUES)
+      func_call_text(args_FUNC_CALL_TEXT "embed_file_as_macro" "${ARGV}")
+      message(
+        WARNING "embed_file_as_macro called as:\n"
+                "${args_FUNC_CALL_TEXT}\n"
+                "HEADER_GUARD specified but not given an argument and will "
+                "default to: ${args_HEADER_GUARD}")
+    endif()
+  endif()
+
+  file(READ ${file_path} file_contents HEX)
+  file(SIZE ${file_path} file_size)
+  string(REGEX MATCHALL "[0-9a-f][0-9a-f]" hex_contents_list ${file_contents})
+
+
+  # Counter to format code to 13 hex bytes per line
+  set(current_hex_per_line 0)
+  set(hex_read_so_far 0)
+  set(byte_array "\n\t{")
+  foreach(hex_byte IN LISTS hex_contents_list)
+    # Could use math(EXPR ...) with output_format HEXADECIMAL ?
+    string(APPEND byte_array "0x${hex_byte}")
+    math(EXPR current_hex_per_line "${current_hex_per_line} + 1")
+    math(EXPR hex_read_so_far "${hex_read_so_far} + 1")
+    if (hex_read_so_far LESS file_size)
+      string(APPEND byte_array ", ")
+    endif()
+    if (current_hex_per_line GREATER 12) # Change to greater equal 13 .....
+      string(APPEND byte_array " \\\n\t")
+      set(current_hex_per_line 0)
+    endif()
+  endforeach()
+
+  message(STATUS "${file_size}")
+  message(STATUS "${byte_array}")
+
+endfunction()
